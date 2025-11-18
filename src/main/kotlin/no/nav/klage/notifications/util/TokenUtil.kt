34f1002a6deb370c94f5cab8372013d7cd3efb4a
Andreas Jonsson <andreas.jonsson@nav.no>
2025-11-18 21:18:11 +0100

    Removed unused code.

--- src/main/kotlin/no/nav/klage/notifications/util/TokenUtil.kt
+++ src/main/kotlin/no/nav/klage/notifications/util/TokenUtil.kt
@@ -1,14 +1,12 @@
 package no.nav.klage.notifications.util
 
 import no.nav.klage.notifications.config.SecurityConfiguration
-import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.springframework.stereotype.Service
 
 @Service
 class TokenUtil(
     private val tokenValidationContextHolder: TokenValidationContextHolder,
-    private val clientConfigurationProperties: ClientConfigurationProperties,
 ) {
 
     companion object {
@@ -16,16 +14,8 @@ class TokenUtil(
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
-    fun getAccessTokenFrontendSent(): String =
-        tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)!!.encodedToken
-
     fun getIdent(): String =
         tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
             ?.jwtTokenClaims?.get("NAVident")?.toString()
             ?: throw RuntimeException("Ident not found in token")
-
-    fun getCallingApplication(): String =
-        tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
-            ?.jwtTokenClaims?.get("azp_name")?.toString()
-            ?: throw RuntimeException("Application not found in token")
 }
