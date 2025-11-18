18aeb4a223865daa95aa8fb0bd4853fb9213d502
Andreas Jonsson <andreas.jonsson@nav.no>
2025-11-18 19:16:39 +0100

    More debug info.

--- src/main/kotlin/no/nav/klage/notifications/config/JacksonConfig.kt
+++ src/main/kotlin/no/nav/klage/notifications/config/JacksonConfig.kt
@@ -1,8 +1,10 @@
 package no.nav.klage.notifications.config
 
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.StreamReadFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -14,7 +16,13 @@ class JacksonConfig {
     @Bean
     @Primary
     fun objectMapper(): ObjectMapper {
-        return jacksonObjectMapper().apply {
+        //FIXME only use in dev for debug
+        val factory = JsonFactory.builder()
+            .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+            .build()
+        return ObjectMapper(factory).apply {
+            // Register Kotlin module for Kotlin-specific features
+            registerKotlinModule()
             // Register module for Java 8 date/time types
             registerModule(JavaTimeModule())
             dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
