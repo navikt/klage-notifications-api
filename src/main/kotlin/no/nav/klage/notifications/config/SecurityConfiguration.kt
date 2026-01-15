5112341743e1e7fada8cdffb5a076c5f6788ce7c
oyvind-wedoe <oyvind.norsted.wedoe@nav.no>
2026-01-15 12:23:49 +0100

    Fix BasicErrorController package in SecurityConfiguration

--- src/main/kotlin/no/nav/klage/notifications/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/klage/notifications/config/SecurityConfiguration.kt
@@ -4,7 +4,7 @@ import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
 
-@EnableJwtTokenValidation(ignore = ["org.springdoc", "org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController"])
+@EnableJwtTokenValidation(ignore = ["org.springdoc", "org.springframework.boot.webmvc.autoconfigure.error.BasicErrorController"])
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
 internal class SecurityConfiguration {
