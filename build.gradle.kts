15f79568c0a6fdca235406664e273368adb43f68
Andreas Jonsson <andreas.jonsson@nav.no>
2026-01-24 10:43:10 +0100

    Newest Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "4.0.1"
+    id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
