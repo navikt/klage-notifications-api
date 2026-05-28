610f3180a4c2bb4138ace60e68a49d592e03f2aa
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-05-28 08:43:45 +0200

    Fix vulnerabilities, while waiting for fix in Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,17 @@ dependencies {
     implementation("io.opentelemetry:opentelemetry-api:${otelVersion}")
     implementation("net.javacrumbs.shedlock:shedlock-spring:${shedlockVersion}")
     implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:${shedlockVersion}")
+
+    //Fix vulnerabilities, while waiting for fix in Spring Boot.
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.22")
+    implementation("io.netty:netty-codec-http:4.2.14.Final")
+    implementation("io.netty:netty-codec-http2:4.2.14.Final")
+    implementation("io.netty:netty-codec-http3:4.2.14.Final")
+    implementation("io.netty:netty-codec-dns:4.2.14.Final")
+    implementation("io.netty:netty-codec-compression:4.2.14.Final")
+    implementation("io.netty:netty-transport-native-epoll:4.2.14.Final")
+    implementation("io.netty:netty-resolver-dns:4.2.14.Final")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage")
         exclude(group = "org.mockito")
